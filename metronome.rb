a67df13bbbd42b5db5bb162f15f6dda9085b77b8
Jason Ardell <ardell@gmail.com>
2011-12-04 20:04:57 -0500

    Get the whole thing working without EventMachine, only Sinatra.

--- metronome.rb
+++ metronome.rb
@@ -1,27 +1,13 @@
 require 'rubygems'
 require 'bundler/setup'
-require 'em-websocket'
-require 'sinatra/base'
-require 'thin'
+require 'sinatra'
 
-EventMachine.run do
+set :public_folder, File.dirname(__FILE__) + '/public'
 
-  class App < Sinatra::Base
-    set :public_folder, File.dirname(__FILE__) + '/public'
-
-    get '/' do
-      erb :index
-    end
-  end
-
-  EventMachine::WebSocket.start(:host => "localhost", :port => 8080, :debug => true) do |ws|
-    ws.onopen    { ws.send "Hello Client!" }
-    ws.onclose   { puts "WebSocket closed" }
-    ws.onmessage { |msg| ws.send Time.now.to_f * 1000     }
-    ws.onerror   { |e|   puts "Error: #{e.message}" }
-  end
-
-  App.run!({:port => 3000})
-
+get '/' do
+  erb :index
 end
 
+get '/time' do
+  return (Time.now.to_f * 1000).to_s
+end
